0efc879d3c85531e9aadd6bafa916ff3a10b7987
KnightWolf2004 <22uec018@lnmiit.ac.in>
2025-07-13 20:07:54 +0530

    add JWT + role checks
    add JWT + role checks in video routes and services

--- app/videos/routes/video_routes.py
+++ app/videos/routes/video_routes.py
@@ -2,28 +2,30 @@ from fastapi import APIRouter, Depends
 from sqlmodel import Session
 
 from app.main import get_session
+from app.users.models.user_model import User
 from app.utils.shared_utils import serialize_list
 from app.videos.schemas.video_schemas import VideoCreate, VideoDelete, VideoResponse, VideoUpdateStatus
 from app.videos.services.video_services import handle_video_delete, handle_video_update, handle_video_upload, handle_videos_read
-
+from app.auth.dependencies.auth_dependencies import admin_required, editor_required
+from app.auth.services.auth_services import get_current_active_user
 
 router = APIRouter(prefix='/videos', tags=['Video'])
 
-@router.post('/upload', response_model=VideoResponse)                               #TODO: Add @employee_required dependency
-def upload_video(*, session: Session = Depends(get_session), video: VideoCreate):
-    video_db = handle_video_upload(session, video)
+@router.post('/upload', response_model=VideoResponse)
+def upload_video(*, current_user: User = Depends(editor_required), session: Session = Depends(get_session), video: VideoCreate):
+    video_db = handle_video_upload(session=session, video=video, uploader=current_user)
     return VideoResponse.model_validate(video_db)
 
 @router.get('/my-videos', response_model=list[VideoResponse])
-def read_uploads(*, session: Session = Depends(get_session)):
-    videos_db = handle_videos_read(session)
+def read_uploads(*, current_user: User = Depends(get_current_active_user), session: Session = Depends(get_session)):
+    videos_db = handle_videos_read(session=session, user=current_user)
     return serialize_list(videos_db, VideoResponse)
 
-@router.patch('/review', response_model=VideoResponse)                                  #TODO: Add @admin_required dependency
-def update_status(*, session: Session = Depends(get_session), video: VideoUpdateStatus):
-    video_db = handle_video_update(session, video)
+@router.patch('/review', response_model=VideoResponse)
+def update_status(*, current_user: User = Depends(admin_required), session: Session = Depends(get_session), video: VideoUpdateStatus):
+    video_db = handle_video_update(session=session, video=video, user=current_user)
     return VideoResponse.model_validate(video_db)
 
-@router.delete('/delete')                                                               #TODO: Add @employee_required dependency
-def delete_video(*, session: Session = Depends(get_session), video: VideoDelete):
-    return handle_video_delete(session, video)
+@router.delete('/delete')
+def delete_video(*, current_user: User = Depends(editor_required), session: Session = Depends(get_session), video: VideoDelete):
+    return handle_video_delete(session=session, video=video, user=current_user)
--- app/videos/services/video_services.py
+++ app/videos/services/video_services.py
@@ -7,16 +7,13 @@ from app.videos.models.video_model import Video
 from app.videos.schemas.video_schemas import VideoCreate, VideoDelete, VideoUpdateStatus
 
 
-def handle_video_upload(session: Session, video: VideoCreate):
-    uploaded_by = 0  # TODO: Get from token
-    reviewed_by = 0  # TODO: Get from token
+def handle_video_upload(session: Session, video: VideoCreate, uploader: User):
+    uploaded_by = uploader.id
+    reviewed_by = uploader.admin_id
 
     extra_data = {"uploaded_by": uploaded_by, "reviewed_by": reviewed_by}
     db_video = Video.model_validate(video, update=extra_data)
 
-    uploader = session.get(User, uploaded_by)
-    if uploader is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not valid account")
     db_video.uploader = uploader
 
     reviewer = session.get(User, reviewed_by)
@@ -27,32 +24,22 @@ def handle_video_upload(session: Session, video: VideoCreate):
     session.add(db_video)
     session.commit()
     session.refresh(db_video)
-
     return db_video
 
-def handle_videos_read(session: Session):
-    db_user = session.get(User, 0)                                                 #TODO: get user id
-    if db_user is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
-    if db_user.role == Role.Admin:
-        videos = db_user.reviewed
-    elif db_user.role == Role.Editor: 
-        videos = db_user.uploaded
+def handle_videos_read(session: Session, user: User):
+    if user.role == Role.Admin:
+        videos = user.reviewed
+    elif user.role == Role.Editor: 
+        videos = user.uploaded
     else:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="No role assigned to user")
     
-    if not videos:
-        return []
-    else:
-        return videos
+    return videos
     
-def handle_video_update(session: Session, video: VideoUpdateStatus):
+def handle_video_update(session: Session, video: VideoUpdateStatus, user: User):
     db_video = session.get(Video, video.id)
     if db_video is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Video not found")
-    user = session.get(User, 0)                                                         #TODO: get user id
-    if user is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
     if db_video.reviewed_by != user.id:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Video not found")
     update = {"status": video.status}
@@ -62,13 +49,10 @@ def handle_video_update(session: Session, video: VideoUpdateStatus):
     session.refresh(db_video)
     return db_video
 
-def handle_video_delete(session: Session, video: VideoDelete):
+def handle_video_delete(session: Session, video: VideoDelete, user: User):
     db_video = session.get(Video, video.id)
     if db_video is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Video not found")
-    user = session.get(User, 0)                                                         #TODO: get user id
-    if user is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="In valid user")
     if db_video.uploaded_by != user.id:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Video not found")
     
