dddc49c8254e132dc394308a15438c69d8a57203
KnightWolf2004 <22uec018@lnmiit.ac.in>
2025-07-14 12:31:49 +0530

    update UserResponse class
    added field_serializer to display roles cleanly

--- app/users/schemas/user_schemas.py
+++ app/users/schemas/user_schemas.py
@@ -1,3 +1,4 @@
+from pydantic import field_serializer
 from sqlmodel import SQLModel
 
 from app.users.models.user_enums import Role
@@ -12,6 +13,10 @@ class UserResponse(SQLModel):
     name: str
     username: str
     role: Role
+
+    @field_serializer('role')
+    def serialize_role(self, role: Role):
+        return role.name
 
 class EditorDelete(SQLModel):
     id: int | None = None
